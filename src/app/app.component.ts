dc4731da9b4faa17c331966e7d111c9cf64096bd
MENKAM Francis <menkam@gmail.com>
2020-03-16 17:43:04 +0100

    add env service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import {NavController, Platform} from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {AuthenticationService} from './services/auth/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthenticationService,
+    private navCtrl: NavController
   ) {
     this.initializeApp();
   }
@@ -22,6 +25,17 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      // laisser la barre d'état superposer la vue Web
+      this.statusBar.overlaysWebView(true);
+      // définir la barre d'état sur bleu
+      this.statusBar.backgroundColorByHexString('#0000FF');
+      this.authService.authenticationState.subscribe(state => {
+        if (state) {
+          this.navCtrl.navigateRoot('/dashboard');
+        } else {
+          this.navCtrl.navigateRoot('/welcome');
+        }
+      });
     });
   }
 }
